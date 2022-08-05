0f75a4af58ca2b521674100e25cbc82b1998be54
shiyunkhoo <shiyun.khoo@gmail.com>
2022-08-05 08:57:48 +0800

    hero 5Aug2022

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+  <div>
+    <header class="grid grid-cols-2 bg-black text-white font-bold">
+      <div id="logo" class="flex flex-row">
+        <img src="/logo.png" class="h-20 w-20 m-5" alt="" />
+        <p class="text-3xl font-bold my-auto">Origami Online</p>
+      </div>
+      <div id="navigation" class="grid grid-cols-4 my-auto text-2xl font-bold">
+        <span>nav item 1</span>
+        <span>nav item 2</span>
+        <span>nav item 3</span>
+        <span>nav item 4</span>
+      </div>
+    </header>
+    <div class="flex h-screen h-[100vh] bg-[url('/hero-bg.jpg')] bg-cover bg-center align-middle text-white text-center">
+      <div class="m-auto p-60 bg-black bg-opacity-30 rounded-full">
+        <h1 class="text-5xl">Origami Online</h1>
+        <p class="mt-5 text-3xl font-bold">fold origami virtually</p>
+      </div>
     </div>
+    <footer class="bg-black text-white m-auto text-center">
+      <p class="py-5 text-2xl font-bold">Made in Malaysia 2022</p>
+    </footer>
   </div>
 </template>
